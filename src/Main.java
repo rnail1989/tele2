bb5e6d396cf9d267ea8c8b8a0fe63867dcb88483
rnail1989 <rnail1989@mail.ru>
2020-10-11 21:48:52 +0300

    tele2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Tariff online = new Tariff();
+
+        System.out.println(online.id);
+        System.out.println(online.name);
+
+
+    }
+}
---
+++ src/Tariff.java
@@ -0,0 +1,16 @@
+public class Tariff {
+    String id;
+
+
+    String name;
+    int price;
+    String totalGb;
+    boolean isVk;
+    boolean isFb;
+    boolean isInts;
+    boolean isWhatsApp;
+    boolean isTg;
+    String buySim;
+    String buyUrl;
+}
+}
