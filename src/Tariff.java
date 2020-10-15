7fce414a268ca1133b65dd0e499ba0cf371a4b7e
rnail1989 <rnail1989@mail.ru>
2020-10-15 18:35:16 +0300

    update Access modifiers

--- src/Tariff.java
+++ src/Tariff.java
@@ -2,11 +2,11 @@ public class Tariff {
     public String id;
     public int price;
     public String totalGb;
-    boolean isVk;
-    boolean isFb;
-    boolean isInts;
-    boolean isWhatsApp;
-    boolean isTg;
+    private boolean isVk;
+    private boolean isFb;
+    private boolean isInts;
+    private boolean isWhatsApp;
+    private boolean isTg;
     String buySim;
     String buyUrl;
 }
